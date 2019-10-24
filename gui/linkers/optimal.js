498f18a4c4fbfb8bb92b16ceb288ff2d747df683
Iftekhar Jamil <jamil.iitdu@gmail.com>
2019-10-25 01:52:45 +0600

    Draft GUI is implemented

---
+++ gui/linkers/optimal.js
@@ -0,0 +1,52 @@
+let {PythonShell} = require('python-shell')
+var path = require("path")
+
+
+function get_weather() {
+  
+
+  var date = document.getElementById("date").value
+  var time1 = document.getElementById("time1").value
+  var time2 = document.getElementById("time2").value
+  // name = city
+  var options = {
+    scriptPath : path.join(__dirname, '/../engine/'),
+    args : [date,time1,time2]
+  }
+
+  let pyshell = new PythonShell('optimal.py', options);
+
+  // debugger;
+  pyshell.on('message', function(message) {
+    // console.log(message);
+     var time = message.split(" ")[0];
+    //  console.log(time)
+     var img = message.split(" ")[1]; 
+     time = Number(time);
+   
+     var decimal = time - Math.floor(time);
+   
+     time = Math.floor(time)+(decimal*60.0)/100.0;
+    //  console.log(message.slice(0,10)); 
+     var h = document.createElement("H1"); 
+      time = time.toFixed(2);
+     time = time.toString();
+     if(time.length==4)
+        time = "0"+time;
+     var t = document.createTextNode("Most optimal time is "+time); 
+     h.appendChild(t);
+     document.getElementById("output").appendChild(h);
+     var elem = document.createElement('img');
+        elem.setAttribute("src", "data:image/jpeg;base64, "+img);
+        elem.setAttribute("height", "400px");
+        elem.setAttribute("width", "600px");
+        elem.setAttribute("id","output");
+      document.getElementById("output").appendChild(elem);
+
+  })
+  
+  document.getElementById("date").value = "";
+  document.getElementById("time1").value = "";
+  document.getElementById("time2").value = "";
+  
+}
---
+++ gui/linkers/prediction.js
@@ -0,0 +1,38 @@
+let {PythonShell} = require('python-shell')
+var path = require("path")
+
+
+function get_weather() {
+
+  var time = document.getElementById("time").value
+  var date = document.getElementById("date").value
+  // name = city
+  var options = {
+    scriptPath : path.join(__dirname, '/../engine/'),
+    args : [time,date]
+  }
+
+  let pyshell = new PythonShell('prediction.py', options);
+
+
+  pyshell.on('message', function(message) {
+     console.log(message);
+     
+     if(document.getElementById("output")!=undefined){
+      var elem1 = document.getElementById("output");
+      elem1.parentNode.removeChild(elem1);
+     }
+        var elem = document.createElement('img');
+        elem.setAttribute("src", "data:image/jpeg;base64, "+message);
+        elem.setAttribute("height", "400px");
+        elem.setAttribute("width", "600px");
+        elem.setAttribute("id","output");
+     
+    // // newImage.src = message;
+     document.getElementById("New").appendChild(elem);
+    // // document.getElementById("New").innerHTML = newImage.outerHTML;
+  })
+  document.getElementById("time").value = "";
+  document.getElementById("date").value = "";
+  
+}
